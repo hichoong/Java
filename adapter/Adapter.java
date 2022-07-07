6bc288e92accfdb92ec2f0d20719778ae9af334b
choong <ksky9230@naver.com>
2022-07-08 01:04:29 +0900

    어댑터 패턴(연관성 없는 두 패턴을 알고리즘 요구사항에 맞춰 사용가능)

---
+++ adapter/Adapter.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public interface Adapter {
+
+    //원하는 기능
+   Float twiceOf(Float f);
+    //원하는 기능
+    Float halfOf(Float f);
+}
---
+++ adapter/AdapterImpl.java
@@ -0,0 +1,16 @@
+package adapter;
+
+public class AdapterImpl implements Adapter{
+
+    @Override
+    public Float twiceOf(Float f) {
+        System.out.println("두배 함수 호출");
+        return Math.doubled(f.doubleValue()).floatValue();
+    }
+
+    @Override
+    public Float halfOf(Float f) {
+        System.out.println("절반 함수 호출");
+        return (float)Math.half(f.doubleValue());
+    }
+}
