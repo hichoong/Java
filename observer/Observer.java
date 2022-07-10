be2627c4e624d893413f70d0ab866634a9c18472
choong <ksky9230@naver.com>
2022-07-10 20:06:01 +0900

    옵저버 패턴(한 객체의 상태가 바뀌면 그 객체에 의존하는 다른 객체들한테 연락이 가고, 자동으로 내용이 갱신되는 방식)

---
+++ observer/Observer.java
@@ -0,0 +1,5 @@
+package observer;
+
+public interface Observer {
+     void update(float tmep, float humidity, float pressure);
+}
---
+++ observer2/WeatherData.java
@@ -0,0 +1,7 @@
+package observer2;
+
+import java.util.Observable;
+
+public class WeatherData extends Observable {
+
+}
