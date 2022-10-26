ce4034ddbdba07c191cf0b614b9dd54c2f5b10c6
choong <ksky9230@naver.com>
2022-10-27 00:53:35 +0900

    프로그래머스 Lv1 핸드폰 번호 가리기

--- algorithm/ex/Solution.java
+++ algorithm/ex/Solution.java
@@ -149,4 +149,17 @@ class Solution {
             }
         return answer;
     }
+
+    //핸드폰 번호 가리기
+    public static String solution11(String phone_number) {
+        String answer = "";
+        for (int i = 0; i < phone_number.length(); i++) {
+            if (i >= phone_number.length() - 4) {
+                answer += phone_number.charAt(i);
+            }else {
+                answer += "*";
+            }
+        }
+        return answer;
+    }
 }
